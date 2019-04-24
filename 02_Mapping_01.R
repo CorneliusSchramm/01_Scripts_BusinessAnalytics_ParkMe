584d5dc7a3d2cbed38296c9602b40556590b45e6
leonidasbothmer <44315917+leonidasbothmer@users.noreply.github.com>
2019-04-24 15:47:35 +0200

    Update 02_Mapping_01.R

--- 02_Mapping_01.R
+++ 02_Mapping_01.R
@@ -11,61 +11,101 @@ library(tidyverse)
 library(ggmap)
 library(dplyr)
 library(ggplot2)
-
+library(gridExtra)
 
 # Clear workspace
 rm(list=ls())
 graphics.off()
 
+# -------
+
+#
+load("../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_01.RData")
+# remove too high too low before saving image
 
 # Reading our data into R
-parking_orig = fread("../02_Business_Analytics_Data/Paid_Parking_Occupancy__Last_30_Days_.csv")
+# parking_orig = fread("../02_Business_Analytics_Data/Paid_Parking_Occupancy__Last_30_Days_.csv")
 
 # Because of OneDrive we need to load from two different paths
-parking_orig = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/Paid_Parking_Occupancy__Last_30_Days_.csv")
+# parking_orig = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/Paid_Parking_Occupancy__Last_30_Days_.csv")
 
-
-# Making locationsDF --------
-locations = data.frame(unique(parking_orig$Location))
-colnames(locations) = c("Location")
-locations$Location = gsub("POINT \\(", "", locations$Location)
-locations$Location = gsub("\\)", "", locations$Location)
-locations = separate(locations, Location, into = c("lon", "lat"), sep=" ")
-locations[,1] = as.numeric(locations[,1])
-locations[,2] = as.numeric(locations[,2])
 
 # Google maps API ----------------
+
+# Register the google api key
 register_google(key="AIzaSyAfPULmtU7hUcoj4lboRAbzVg-810wrkJs")
 
-map.seattle_city <- qmap("seattle", zoom = 11, source="stamen", maptype="toner",darken = c(.3,"#BBBBBB"))
-map.seattle_city
 
-map = get_map("Seattle", zoom = 13)
-ggmap(map)
+# Create mapping data and function -----------------
 
-ggmap(map) + 
-  geom_point(data=locations, mapping= aes(x=lon, y=lat),size=.5) +
-  xlim()
+create_map = function(d, t) {
+  # Creating dataframe "locations" with locations of all 1477 parking meters
+  locations = data.frame(parking_orig[!duplicated(parking_orig[,c("SourceElementKey","lon","lat")]),][,c(6,12,13)])
+  locations[,2] = as.numeric(locations[,2])
+  locations[,3] = as.numeric(locations[,3])
+  # hierbei gehen Nachkommastellen verloren ?!
+  
+  # locations = data.frame(unique(parking_orig$Location))
+  # colnames(locations) = c("Location")
+  # locations$Location = gsub("POINT \\(", "", locations$Location)
+  # locations$Location = gsub("\\)", "", locations$Location)
+  # locations = separate(locations, Location, into = c("lon", "lat"), sep=" ")
+  # locations[,1] = as.numeric(locations[,1])
+  # locations[,2] = as.numeric(locations[,2])
+  
+  # locations_2 = data.frame(unique(parking_orig$SourceElementKey))
+  # colnames(locations_2) = c("parkingMeterId")
+  # locations_2["Locations"] = NA
+  
+  # Get columns freePercent and ParkingSpaceCount for specific date
+  # For "2019-03-15" "15:00"
+  date_filtered = parking_orig %>%
+    filter(date == d) %>%
+    filter(time == t)
+  date_filtered = date_filtered[,c(6,8,18)]
+  
+  # Merge both together by 
+  df_plot = merge(locations, date_filtered, by="SourceElementKey", all=TRUE)
+  rm(locations, date_filtered)
   
   
-  # xlim(min(locations$lat), max(locations$lat)) +
-  # ylim(min(locations$lon), max(locations$lon))
+  
+  # # Mapping ith QMAP
+  # map = qmap("seattle", 
+  #            zoom = 13, 
+  #            source = "stamen", 
+  #            maptype = "toner",
+  #            darken = c(.3,"#BBBBBB")
+  #            )
+  
+  map = get_map("Seattle",
+                zoom = 13,
+                maptype = "toner-lite") # Others are toner-lite, terrain-lines
+  
+  # Mapping with ggmmap and ggplot
+  ggmap(map) + 
+    geom_point(data = df_plot, 
+               mapping = aes(x = lon,
+                             y = lat,
+                             color = freePercent
+                             #,size = ParkingSpaceCount
+                             ),
+               alpha=1
+               #,size=2
+               ) +
+    # Limits on y and x-axis: excluding some values
+    ylim(47.59, 47.64) +
+    xlim(-122.375, -122.3) +
+    # Set color gradient
+    scale_colour_gradient(low = "red2", high = "green3",                           # Choose better colors
+                          space = "Lab", na.value = "grey50", guide = "colourbar",
+                          aesthetics = "colour")
+    # Styling
+    # Remove Axis tics etc
+}
 
-ggplot() +
-  geom_point(data=locations, mapping= aes(x=lon, y=lat),size=0.5)
+# Create maps for different times and plot next to each other
+p1 = create_map("2019-03-15", "12:00")
+p2 = create_map("2019-03-18", "15:00")
+grid.arrange(p1, p2, ncol=2)
 
-
-########
-length(unique(parking_orig$SourceElementKey))
-
-parking_one = parking_orig %>%
-  filter(SourceElementKey==11670)
-
-parking_one = separate(parking_one, OccupancyDateTime, into = c("date", "time", "am/pm"), sep=" ")
-
-
-# Edit time format
-parking_one$time = paste0(parking_one$time, " ", parking_one$`am/pm`)
-parking_one$time = format(strptime(parking_one$time, "%I:%M:%S %p"), format="%H:%M")
-parking_one = parking_one %>%
-  filter(date=="03/18/2019")
