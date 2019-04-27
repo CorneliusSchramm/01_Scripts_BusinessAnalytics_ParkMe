44a9cc59bf8e66769b5466799145cc5740ef1e39
CSchramm <cornelius.schramm@student.unisg.ch>
2019-04-27 13:59:23 +0200

    Revert "lala"
    This reverts commit 937eeba5f9c2de6f75c88bc28bbd3e24d18d8553.

--- 03_Analysis_01.R
+++ 03_Analysis_01.R
@@ -13,7 +13,7 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(forecast)
-library("tseries")
+
 # Clear workspace
 rm(list=ls())
 graphics.off()
@@ -23,7 +23,7 @@ graphics.off()
 theme_set(theme_minimal())
 
 # Load the previousely saved version of our parking data as well as new data (weather, events)
-load("../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_02_.RData")
+load("../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_01.RData")
 # dates = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/dates.csv")
 # holidays = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/holidays.csv")
 # weather_01 = read_csv2("../02_Business_Analytics_Data/weather.csv") #(25th March to 22nd April)
@@ -31,7 +31,7 @@ load("../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_02_.RData")
 # events = fread("../02_Business_Analytics_Data/events.csv")
 
 # Because of OneDrive we need to load from two different paths
-load("../02_Business_Analytics_Data/df_set_02_merged.RData")
+load("../02_Business_Analytics_Data/df_set_01.RData")
 # dates = fread("../02_Business_Analytics_Data/dates.csv")
 # holidays = fread("../02_Business_Analytics_Data/holidays.csv")
 # weather_01 = read_csv2("../Schramm, Cornelius - 02_Business_Analytics_Data/weather.csv") #(25th March to 22nd April)
@@ -43,8 +43,10 @@ load("../02_Business_Analytics_Data/df_set_02_merged.RData")
 # Choose parking meter
 parkingmeter = 13105
 
+# Remove unnecessary data
+rm(too_high_index, too_low_index)
+
 # Filter one parking meter
-parking_orig = DF_final_small
 parking_filtered = parking_orig %>%
   filter(SourceElementKey == parkingmeter)
 
@@ -52,8 +54,7 @@ parking_filtered = parking_orig %>%
 parking_filtered$datetime = paste(parking_filtered$date, parking_filtered$time)
 parking_filtered = parking_filtered %>%
   select(datetime, everything())
-# Right format
-parking_filtered$datetime = as.POSIXct(parking_filtered$datetime, format="%Y-%m-%d %H:%M")
+parking_filtered$datetime = as.POSIXct(paste(parking_filtered$date, parking_filtered$time), format="%Y-%m-%d %H:%M")
 
 # Order by date and time
 parking_filtered = parking_filtered[order(parking_filtered$datetime),]
@@ -66,61 +67,43 @@ rownames(parking_filtered) = NULL
 # Analysis -----
 
 # Remove unwanted columns
-parking_filtered = parking_filtered[, c(1,6,24)]
+parking_filtered = parking_filtered[, c(1,2,19)]
 
 
 # Plot parking density over one day
 # Choose date
-example_date1 = "2019-03-25"
-
+example_date1 = "2019-03-15"
 data_plot = parking_filtered %>%
-  filter(parking_filtered$date.x == example_date1)
-
+filter(parking_filtered$date == example_date1)
 ggplot(data_plot) +
   geom_line(aes(x=datetime, y=freePercent))
-
 # Plot parking density over two days
 # Choose dates
-example_date2 = "2019-03-30"
+example_date2 = "2019-03-16"
 data_plot = parking_filtered %>%
-  filter(date.x >= example_date1 & date.x <= example_date2)
-
+  filter(parking_filtered$date == example_date1 | parking_filtered$date == example_date2)
 ggplot(data_plot) +
   geom_line(aes(x=datetime, y=freePercent))
 
 # Separate into training and testing data
-train_start = "2019-03-25"
-train_end = "2019-03-27"
-test_start = "2019-03-28"
-test_end = "2019-03-28"
+data_train = parking_filtered[1:(0.75*nrow(parking_filtered)),c(1,3)]
+data_test = parking_filtered[-(1:(0.75*nrow(parking_filtered))),c(1,3)]
 
-
-
-data_train = parking_filtered %>%
-  filter(date.x >= train_start & date.x <= train_end)
-data_train = data_train[,-2]
-
-data_test = parking_filtered %>%
-  filter(date.x >= test_start & date.x <= test_end)
-data_test = data_test[,-2]
 # Forecast model via "forecast" package
-
 ts = ts(data = data_train,
-           start = c(2019, 3, 25, 8, 0))
+           frequency = 525600,
+           start = c(2019, 3, 15, 12, 0))
 
 plot.ts(ts)
 
-model = auto.arima(ts[,2], seasonal = T)
-
-tsdisplay(residuals(fit), lag.max=45, main='(1,1,1) Model Residuals')
-
+model = auto.arima(ts[,2])
 summary(model)
 
 # Forecast 
 # forcast days
-fc_days = 1
+fc_days = 5
 fc_time = fc_days*24*60
-fc = forecast(model)
+fc = forecast(model, h=fc_time)
 
 plot(fc)
 
