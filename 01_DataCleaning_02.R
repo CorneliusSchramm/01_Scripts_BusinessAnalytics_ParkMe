56c88987115f45a2cf259f245be3d7f691ff62fb
CHarprecht <43964402+CHarprecht@users.noreply.github.com>
2019-04-27 16:22:16 +0200

    aufräumen

--- 01_DataCleaning_02.R
+++ 01_DataCleaning_02.R
@@ -7,11 +7,11 @@
 # Setup ----------------------------------------------
 
 # Load required packages
-library(data.table)
+
 library(tidyverse)
-
-
 library(dplyr)
+library(data.table)
+library(tidyr)
 library(ggplot2)
 
 
@@ -20,56 +20,110 @@ rm(list=ls())
 graphics.off()
 
 # Load the previousely saved version of our parking data as well as new data (weather, events)
-load("../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_01.RData")
-dates = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/dates.csv")
-holidays = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/holidays.csv")
-weather_01 = read_csv2("../02_Business_Analytics_Data/weather.csv") #(25th March to 22nd April)
-# weather_02 = read_csv2("../02_Business_Analytics_Data/history_export_2019-04-08T16_02_14.csv", skip =11)
-# events = fread("../02_Business_Analytics_Data/events.csv")
+load("../02_Business_Analytics_Data/df_set_01.RData")
+events = fread("../02_Business_Analytics_Data/Special_Events_Permits.csv")
+weather = fread("../02_Business_Analytics_Data/weather.csv", header = T)
+#dates = fread("../02_Business_Analytics_Data/dates.csv")
+#holidays = fread("../02_Business_Analytics_Data/holidays.csv")
+# weather_2 = fread("../02_Business_Analytics_Data/history_export_2019-04-22T09_14_54.csv", header = T, sep = ";", skip = 11)
+# weather_1 = fread("../02_Business_Analytics_Data/history_export_2019-04-08T16_02_14.csv", header = T, sep = ";", skip = 11)
+
+
 
 # Because of OneDrive we need to load from two different paths
-load("../02_Business_Analytics_Data/df_set_01.RData")
-dates = fread("../02_Business_Analytics_Data/dates.csv")
-holidays = fread("../02_Business_Analytics_Data/holidays.csv")
-weather_01 = read_csv2("../Schramm, Cornelius - 02_Business_Analytics_Data/weather.csv") #(25th March to 22nd April)
-# weather_02 = read_csv2("../Schramm, Cornelius - 02_Business_Analytics_Data/history_export_2019-04-08T16_02_14.csv", skip =11)
-# events = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/events.csv")
-
-# Clean "dates" Dataset ------------------------------
-
-# Rename columns
-colnames(dates) = c(
-  "date",
-  "day",
-  "is_weekend",
-  "is_fr/sa"
-)
-# Edit date format
-dates$date = as.Date(dates$date, "%d.%m.%y")
+load("../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_01.RData")
+events = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/Special_Events_Permits.csv")
+weather = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/weather.csv", header = T)
+#dates = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/dates.csv")
+#holidays = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/holidays.csv")
+#weather_2 = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/history_export_2019-04-22T09_14_54.csv", header = T, sep = ";", skip = 11)
+#weather_1 = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/history_export_2019-04-08T16_02_14.csv", header = T, sep = ";", skip = 11)
 
 
-# Clean "holidays" Dataset ---------------------------
+# Cleaning ---------------------
 
-# Omitt empty rows and columns
-holidays = holidays[c(2:13), c(1,2,3)]
-# Rename columns
-colnames(holidays) = c(
-  "date",
-  "holiday_name",
-  "is_holiday"
-)
-# Edit date format
-holidays$date = as.Date(holidays$date, "%d.%m.%y")
+# Clean Event Data 
+events = separate(events,`Event End Date`, c("End.date", "End.time", "End AM/PM"), sep=" ")
+events$End.date = as.Date(events$End.date, "%m/%d/%Y")
+events = separate(events,`Event Start Date`, c("Start.date", "Start.time", "Start AM/PM"), sep=" ")
+events$Start.date = as.Date(events$Start.date, "%m/%d/%Y")
+colnames(events)[8] = "date.x"
 
-# Clean "weather" Dataset ----------------------------
+# # Differ between events that have been granted and events that are in process
+# complete_index = which(events$`Permit Status`== "Complete")
+# events = events[complete_index]
 
-# Merge/stack both weather datasets
-# Not done here because the first one has less columns (?)
+# Check wether there are events that last longer than a day
+events = events[events$date.x >= "2019-03-25" & events$date.x <= "2019-04-22",]
+which((events$date.x != events$End.date))
 
-weather = weather_02
-# Create columns "date" and "time" in common format
-weather$date = paste0(weather$Day, ".", weather$Month, ".", weather$Year)
-weather$date = as.Date(weather$date, "%d.%m.%Y")
-weather$time = paste0(weather$Hour, ":", weather$Minute)
-# Sort columns
-weather = select(weather, date, time, everything())
+# Choose relevant columns
+events = events[, c(8, 15, 19)]
+
+# ...
+events = separate_rows(events, `Event Location - Neighborhood`, convert = TRUE, sep=" / ")
+events = separate_rows(events, `Event Location - Neighborhood`, convert = TRUE, sep=";")
+
+# Clean Holiday
+# holidays = holidays[-1, -c(3,4) ]
+
+# Clean weather data
+weather$date = as.Date(with(weather, paste(Year, Month, Day, Hour, sep="-")), "%Y-%m-%d")
+# Making Merge Column
+weather = transform(weather, MergCol = paste(date, Hour, sep="_"))
+
+parking = parking_orig[parking_orig$date>= "2019-03-25" & parking_orig$date <= "2019-04-1",]
+# Clean parking -> make merge Column
+parking$hour = as.numeric(substr(parking$time, start = 1, stop = 2))
+parking = transform(parking, MergCol=paste(date, hour, sep="_"))
+# Same for parking_orig
+# memory.limit(70000) ## So we dont get an error due to too big of an vector 
+# parking_orig$hour = as.numeric(substr(parking_orig$time, start = 1, stop = 2))
+# parking_orig = transform(parking_orig, MergCol=paste(date, hour, sep="_"))
+
+
+# Merging --------------------
+
+
+
+# Merging weather and parking----------
+DF_merged_small_v1 = merge(parking, weather, 
+                           by="MergCol", all.x=TRUE)
+# Same for parking_orig
+# DF_merged_large_v1 = merge(parking_orig, weather, 
+#                            by="MergCol", all.x=TRUE)
+
+# Merging DF_merged_small_v1 and events
+DF_merged_small_v2 = merge(DF_merged_small_v1, events, 
+                           by.x=c("date.x","PaidParkingArea"), 
+                           by.y=c("date.x","Event Location - Neighborhood"),
+                           all.y=F, all.x=T)
+# Same for parking_orig
+# DF_merged_large_v2 = merge(DF_merged_large_v1,events, by="date.x", all.x=TRUE)
+
+# Getting Weekday -----
+
+DF_merged_small_v2$Weekday = weekdays(DF_merged_small_v1$date.x)
+DF_merged_small_v2[DF_merged_small_v2$Weekday == "Samstag" | DF_merged_small_v2$Weekday == "Sonntag","is_we"] = 1
+DF_merged_small_v2[is.na(DF_merged_small_v2$is_we),"is_we"] = 0
+
+# Same for parking_orig
+# DF_merged_large_v2$Weekday = weekdays(DF_merged_large_v1$date.x)
+
+# Sorting Columns
+DF_final_small = DF_merged_small_v2[,c(8,13,14,41,1,42,4,9,12,17,26:35,40,10,19)]
+
+#WHat does is this line supposed to do?
+#DF_final_small = DF_final_small[DF_final_small$date.x >= "2019-03-25" & DF_final_small$date.x <= "2019-04-22",]
+
+# DF_final_large = DF_merged_large_v2[,c(8,13,14,41,1,42,4,9,12,17,26:35,40,10,19)]
+
+#What is this supposed to do?
+# DF_final_large = DF_final_large[DF_final_large$date.x >= "2019-03-25" & DF_final_large$date.x <= "2019-04-22",]
+
+rm(dates, DF_merged_small_v1, DF_merged_small_v2, 
+   events, holidays,parking, parking_orig, weather, 
+   too_high_index, too_low_index)
+
+save.image(file = "../02_Business_Analytics_Data/df_set_02_merged.RData")
+save.image(file = "../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_02_merged.RData")
--- 03_Analysis_01.R
+++ 03_Analysis_01.R
@@ -13,7 +13,7 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(forecast)
-
+library("tseries")
 # Clear workspace
 rm(list=ls())
 graphics.off()
@@ -23,7 +23,7 @@ graphics.off()
 theme_set(theme_minimal())
 
 # Load the previousely saved version of our parking data as well as new data (weather, events)
-load("../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_01.RData")
+load("../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_02_merged.RData")
 # dates = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/dates.csv")
 # holidays = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/holidays.csv")
 # weather_01 = read_csv2("../02_Business_Analytics_Data/weather.csv") #(25th March to 22nd April)
@@ -31,7 +31,7 @@ load("../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_01.RData")
 # events = fread("../02_Business_Analytics_Data/events.csv")
 
 # Because of OneDrive we need to load from two different paths
-load("../02_Business_Analytics_Data/df_set_01.RData")
+load("../02_Business_Analytics_Data/df_set_02_merged.RData")
 # dates = fread("../02_Business_Analytics_Data/dates.csv")
 # holidays = fread("../02_Business_Analytics_Data/holidays.csv")
 # weather_01 = read_csv2("../Schramm, Cornelius - 02_Business_Analytics_Data/weather.csv") #(25th March to 22nd April)
@@ -43,10 +43,8 @@ load("../02_Business_Analytics_Data/df_set_01.RData")
 # Choose parking meter
 parkingmeter = 13105
 
-# Remove unnecessary data
-rm(too_high_index, too_low_index)
-
 # Filter one parking meter
+parking_orig = DF_final_small
 parking_filtered = parking_orig %>%
   filter(SourceElementKey == parkingmeter)
 
@@ -54,7 +52,8 @@ parking_filtered = parking_orig %>%
 parking_filtered$datetime = paste(parking_filtered$date, parking_filtered$time)
 parking_filtered = parking_filtered %>%
   select(datetime, everything())
-parking_filtered$datetime = as.POSIXct(paste(parking_filtered$date, parking_filtered$time), format="%Y-%m-%d %H:%M")
+# Right format
+parking_filtered$datetime = as.POSIXct(parking_filtered$datetime, format="%Y-%m-%d %H:%M")
 
 # Order by date and time
 parking_filtered = parking_filtered[order(parking_filtered$datetime),]
@@ -67,45 +66,63 @@ rownames(parking_filtered) = NULL
 # Analysis -----
 
 # Remove unwanted columns
-parking_filtered = parking_filtered[, c(1,2,19)]
+parking_filtered = parking_filtered[, c(1,6,24)]
 
 
 # Plot parking density over one day
 # Choose date
-example_date1 = "2019-03-15"
+example_date1 = "2019-03-25"
+
 data_plot = parking_filtered %>%
-filter(parking_filtered$date == example_date1)
+  filter(parking_filtered$date.x == example_date1)
+
 ggplot(data_plot) +
   geom_line(aes(x=datetime, y=freePercent))
+
 # Plot parking density over two days
 # Choose dates
-example_date2 = "2019-03-16"
+example_date2 = "2019-03-30"
 data_plot = parking_filtered %>%
-  filter(parking_filtered$date == example_date1 | parking_filtered$date == example_date2)
+  filter(date.x >= example_date1 & date.x <= example_date2)
+
 ggplot(data_plot) +
   geom_line(aes(x=datetime, y=freePercent))
 
 # Separate into training and testing data
-data_train = parking_filtered[1:(0.75*nrow(parking_filtered)),c(1,3)]
-data_test = parking_filtered[-(1:(0.75*nrow(parking_filtered))),c(1,3)]
+train_start = "2019-03-25"
+train_end = "2019-03-30"
+test_start = "2019-04-01"
+test_end = "2019-04-01"
 
+
+
+data_train = parking_filtered %>%
+  filter(date.x >= train_start & date.x <= train_end)
+data_train = data_train[,-2]
+
+data_test = parking_filtered %>%
+  filter(date.x >= test_start & date.x <= test_end)
+data_test = data_test[,-2]
 # Forecast model via "forecast" package
-ts = ts(data = data_train,
-           frequency = 525600,
-           start = c(2019, 3, 15, 12, 0))
 
-plot.ts(ts)
+ts = ts(data = data_train, frequency = 600)
+msts = msts(data_train, seasonal.periods=c(6, 600))
+cycle(msts)
 
-model = auto.arima(ts[,2])
+plot.ts(msts)
+
+model = auto.arima(ts[,2], seasonal = T)
+
+tsdisplay(residuals(fit), lag.max=45, main='(1,1,1) Model Residuals')
+
 summary(model)
 
 # Forecast 
 # forcast days
-fc_days = 5
+fc_days = 1
 fc_time = fc_days*24*60
-fc = forecast(model, h=fc_time)
+fc = forecast(model)
 
 plot(fc)
-
 
 
