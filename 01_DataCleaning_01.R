1c79784ea76d252a79def46b2313d8c4d71cfc83
CSchramm <cornelius.schramm@student.unisg.ch>
2019-05-04 00:34:26 +0200

    Stündlich aggregierter DF steht

--- 01_DataCleaning_01.R
+++ 01_DataCleaning_01.R
@@ -18,9 +18,9 @@ rm(list=ls())
 graphics.off()
 
 # Reading our data into R
-parking_orig = fread("../02_Business_Analytics_Data/Paid_Parking_Occupancy__Last_30_Days_.csv")
+parking_orig = fread("../02_Business_Analytics_Data/Paid_Parking_Occupancy__Last_30_Days_ (1).csv")
 # Because of OneDrive we need to load from two different paths
-parking_orig = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/Paid_Parking_Occupancy__Last_30_Days_.csv")
+parking_orig = fread("../Schramm, Cornelius - 02_Business_Analytics_Data/Paid_Parking_Occupancy__Last_30_Days_ (1).csv")
 
 
 # Cleaning "parking_orig" -----------------
@@ -50,10 +50,41 @@ parking_orig$time = format(strptime(parking_orig$time, "%I:%M:%S %p"), format="%
 # Omitt am/pm column
 parking_orig = parking_orig[,-3]
 
+# Create FreeSpots Column
+parking_orig$FreeSpots = parking_orig$ParkingSpaceCount - parking_orig$PaidOccupancy
+
+
 # Aggregate -----
 
 # Aggregate per hour
-# CODE FEHLT
+
+# making hour column
+parking_orig$hour = substr(parking_orig$time, start = 1, stop = 2)
+
+
+#Removing unnecesary Columns but create copyDF of important information to merge it later
+parking_InfoCopy = parking_orig[,c(6,1,18,7:13,16)]
+
+#making mergeCol
+parking_InfoCopy = transform(parking_InfoCopy, MergCol=paste(date, hour,SourceElementKey ,sep="_"))
+
+test = data.frame(parking_InfoCopy[!duplicated(parking_InfoCopy[,"MergCol"]),][,c(4:12)])
+
+parking_orig = parking_orig[,c(1,6,17,18)]
+
+# Aggregating FreeSpots
+hourly_mean = aggregate(parking_orig$FreeSpots,
+                        by = list(SourceElementKey = parking_orig$SourceElementKey, 
+                                  date = parking_orig$date, 
+                                  hour = parking_orig$hour),
+                        FUN = mean)
+#making mergeCol
+hourly_mean = transform(hourly_mean, MergCol=paste(date, hour,SourceElementKey ,sep="_"))
+
+# Merging back together
+
+DF_hourly= hourly_mean %>%
+  left_join(test, by= "MergCol")
 
 
 # Save -----
@@ -61,5 +92,5 @@ parking_orig = parking_orig[,-3]
 # Remove unnecessary dataframes
 # rm( # FILL DIS)
 
-# save.image(file = "../02_Business_Analytics_Data/df_set_01.RData")
+#save.image(file = "../02_Business_Analytics_Data/df_set_01.RData")
 # save.image(file = "../Schramm, Cornelius - 02_Business_Analytics_Data/df_set_01.RData")
