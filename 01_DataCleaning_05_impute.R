64519e72b25de485623390eb7177518b7c793cf1
CSchramm <cornelius.schramm@student.unisg.ch>
2019-05-08 15:59:50 +0200

    start imputin

--- 01_DataCleaning_05_impute.R
+++ 01_DataCleaning_05_impute.R
@@ -19,22 +19,45 @@ library(ggmap)
 rm(list=ls())
 graphics.off()
 
-# Register Google Key
-register_google(key="AIzaSyAfPULmtU7hUcoj4lboRAbzVg-810wrkJs")
-
 # Load the previousely saved merged version of our parking data
+load("../02_Business_Analytics_Data/pm_kmClust_relation.RData")
+load("../02_Business_Analytics_Data/pmTsOv.RData")
+
+
+# Because of Onedrive
 load("../Schramm, Cornelius - 02_Business_Analytics_Data/pm_kmClust_relation.RData")
 load("../Schramm, Cornelius - 02_Business_Analytics_Data/pmTsOv.RData")
 
 
-#
-# Create name vector for every parking meter in every cluster
-parking_filtered = DF_clustered_slim %>%
-  filter(cluster == 1)
-names = unique(parking_filtered$SourceElementKey)
+# Create name vector for every parking meter in every cluster ------------
+referenceDF = data.frame(DF_clustered_slim[!duplicated(DF_clustered_slim[,"SourceElementKey"]),][,c(1:2)])
+ClusterList = as.numeric(sort(unique(referenceDF$cluster)))
 
+# For Loop to get every Cluster and their corresponding ParkingMeters in sepreates dataframes
+count = 0
+for (a_clust in ClusterList ) {
+  tempVec = as.character(filter(referenceDF,cluster == a_clust)[,-2])
+  tempDF = OV_DF %>%
+    select(datetime, tempVec)
+  tempDF$hour = as.numeric(substr(tempDF$datetime, start = 12, stop = 13))
+  tempDF = tempDF %>%
+    filter(hour >= 8 & hour <= 17)
+  tempDF$Weekday = weekdays(tempDF$datetime)
+  RMindex = which(tempDF$Weekday == "Sonntag")
+  tempDF = tempDF[-RMindex, -c(ncol(tempDF), (ncol(tempDF)-1))]
+  tempDF = tempDF[, colMeans(is.na(tempDF)) < .20] 
+  count = count + ncol(tempDF) # to see how many parking meters were dropped-> 179
+  assign(paste0("Cluster",a_clust), tempDF )
+}
 
+# Imputing ----------
 
+# Step 1: Make all values relative
 
-
+for (i in seq(2,ncol(Cluster1),1)) {
+  Cluster1_perc <<- Cluster1
+  tempSEK = as.numeric(colnames(Cluster1_perc)[i])
+  tempPMCnt = filter(DF_clustered_slim, SourceElementKey == tempSEK)[1,5]
+  Cluster1_perc[,i] = Cluster1_perc[,i]/ tempPMCnt
+}  
 
